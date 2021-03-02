67b076d512438ac995665882f24b116bd2a25cb1
Adrian Quej <Adrian Quej@LAPTOP-LCPEV7PP>
2021-03-01 22:15:17 -0600

    PRIMER COMMIT

---
+++ src/tiposdedatos59903/TiposDeDatos59903.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdedatos59903;
+
+/**
+ *
+ * @author Adrian Quej
+ */
+public class TiposDeDatos59903 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Valores Maximos y Minimos del tipo de dato Byte
+     
+        byte datoTipoByte = 50;
+       
+                    
+        System.out.println("Los valores Maximos y minimos del dato Byte son");
+        System.out.println("Minimo =" +Byte.MIN_VALUE);
+        System.out.println("Maximo =" + Byte.MAX_VALUE);
+        
+        //Valores Maximos y Minimos del tipo de dato Short
+        
+        short datoTipoShort = 17;
+        System.out.println("Los valores Maximos y minimos del dato Short son");
+        System.out.println("Minimo =" +Short.MIN_VALUE);
+        System.out.println("Maximo =" + Short.MAX_VALUE);
+       
+        //Valores Maximos y Minimos del tipo de dato Int
+        int datoTipoInt = 12;
+        System.out.println("Los valores Maximos y minimos del dato Int son");
+        System.out.println("Minimo =" + Integer.MIN_VALUE);
+        System.out.println("Maximo =" + Integer.MAX_VALUE);
+     
+        //Valores Maximos y minimos del tipo de dato Long
+        
+        long datoTipoLong = 36;
+        System.out.println("Los valores Maximos y minimos del dato Long son");
+        System.out.println("Minimo =" + Long.MIN_VALUE);
+        System.out.println("Maximo =" + Long.MAX_VALUE);
+        
+        //Valores Maximos y Minimos del tipo de dato Double
+        
+        double datoTipoDouble = 42;
+        System.out.println("Los valores Maximos y minimos del dato Double son");
+        System.out.println("Minimo =" + Double.MIN_VALUE);
+        System.out.println("Maximo =" + Double.MAX_VALUE);
+    }
+    
+    
+}
